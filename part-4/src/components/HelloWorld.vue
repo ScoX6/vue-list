213f50f98b318c766879b88f1ad612bf9cd9edcf
xzf <yinuo@xzf.com>
2023-12-17 19:59:00 +0800

    update

---
+++ part-4/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h3 v-if="ifFlag1">条件渲染</h3>
+  <h3 v-else>ifFlag1为假</h3>
+  <h3 v-if="ifFlag2">ifFlag2为真</h3>
+  <h3 v-if="ifFlag3==1">ifFlag3 为1</h3>
+  <h3 v-else-if="ifFlag3==2">ifFlag3 为2</h3>
+  <h3 v-else>ifFlag3 既不为1也不为2</h3>
+  <h3 v-show="showFlag">showFlag为真</h3>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        ifFlag1: false,
+        ifFlag2: true,
+        ifFlag3: 3,
+        showFlag: false,
+      }
+    },
+  } 
+</script>
