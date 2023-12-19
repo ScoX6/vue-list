771d7c9c008c0615ff46b491a43a04b9365f1bd6
xzf <yinuo@xzf.com>
2023-12-19 19:47:26 +0800

    update

--- part-7/src/components/HelloWorld.vue
+++ part-7/src/components/HelloWorld.vue
@@ -1,7 +1,26 @@
 <template>
-  
+  <button @click="count++">Add by inner</button>
+  <br>
+  <button @click="methodCountAdd">Add by method</button>
+  <br>
+  <button @dblclick="methodCountAdd2">Add 2 by method</button>
+  <p>count: {{ count }}</p>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      count: 1,
+    }
+  },
+  methods: {
+    methodCountAdd() {
+      this.count++;
+    },
+    methodCountAdd2() {
+      this.count += 2;
+    },
+  }
+}
 </script>
