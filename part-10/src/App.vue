802786d02b1360f5449868d541e4b1440c21ee26
xzf <yinuo@xzf.com>
2023-12-20 14:49:25 +0800

    update

---
+++ part-10/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ArrayList from './components/ArrayList.vue'
+</script>
+
+<template>
+  <ArrayList></ArrayList>
+</template>
---
+++ part-10/src/components/ArrayList.vue
@@ -0,0 +1,27 @@
+<template>
+    <p>数组变化侦听</p>
+    <ul>
+        <li v-for="(item, index) of names" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="addJohn">添加 John</button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            names: [
+                "Rose", "Tom", "Jack", "Taylor"
+            ]
+        }
+    },
+    methods: {
+        addJohn() {
+            this.names.push("John");
+            // this.names.concat(["John"]); // 不会引起UI自动刷新
+            // this.names = this.names.concat(["John"]);
+        }
+    }
+}
+</script>
+
