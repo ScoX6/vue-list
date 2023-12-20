39beffe7b6a3f725bcaf0d5887b82ce927f4ce71
xzf <yinuo@xzf.com>
2023-12-20 16:14:17 +0800

    update

---
+++ part-11/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ComputedDemo from './components/ComputedDemo.vue'
+</script>
+
+<template>
+  <ComputedDemo></ComputedDemo>
+</template>
---
+++ part-11/src/components/ComputedDemo.vue
@@ -0,0 +1,32 @@
+<template>
+  <h3>{{ user.name }}</h3>
+  <p>{{ hobbyContent }}</p>
+  <p>{{ hobbyContentWithMethod() }}</p>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                name: "Tom",
+                age: 12,
+                avatar: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww",
+                gender: "male",
+                hobby: [
+                    "PingPong", "Basketball", "Football"
+                ],
+            }
+        }
+    },
+    methods: {
+        hobbyContentWithMethod() {
+            return this.user.hobby.length > 0 ? "YES" : "No";
+        }
+    },
+    computed: {
+        hobbyContent() {
+            return this.user.hobby.length > 0 ? "YES" : "NO";
+        }
+    }
+}
+</script>
