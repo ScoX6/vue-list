5dda6a8967bc6dc016ec8be934aed2077690de49
xzf <yinuo@xzf.com>
2023-12-21 16:18:27 +0800

    update

---
+++ part-18/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import Header from './pages/Header.vue'
+import Main from './pages/MainBody.vue'
+import Aside from './pages/Aside.vue'
+export default {
+  components: {
+    Header,
+    Main,
+    Aside
+  }
+}
+</script>
+
+<template>
+  <Header/>
+  <Main/>
+  <Aside/>
+</template>
