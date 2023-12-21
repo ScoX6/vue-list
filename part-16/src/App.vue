d865e1ec03855ea245ed797df5d2f0d091c63a17
xzf <yinuo@xzf.com>
2023-12-21 14:50:46 +0800

    update

---
+++ part-16/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ModuleRefDemo from './components/ModuleRefDemo.vue'
+</script>
+
+<template>
+  <ModuleRefDemo/>
+</template>
---
+++ part-16/src/components/ModuleRefDemo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div ref="container" class="container">{{ content }}</div>
+  <button @click="getElementHandle">获取container元素</button>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      content: "内容"
+    }
+  },
+  methods: {
+    getElementHandle() {
+      console.log(this.$refs.container);
+      this.$refs.container.innerHTML = "哈哈哈";
+    }
+  }
+}
+</script>
+
+
+
