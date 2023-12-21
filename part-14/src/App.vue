7c39a61f263391052c444d63bc89dc7e9cecc95d
xzf <yinuo@xzf.com>
2023-12-21 14:13:03 +0800

    update

---
+++ part-14/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import WatchDemo from './components/WatchDemo.vue'
+</script>
+
+<template>
+  <WatchDemo></WatchDemo>
+</template>
---
+++ part-14/src/components/WatchDemo.vue
@@ -0,0 +1,25 @@
+<template>
+  <h3>侦听器</h3>
+  <p>{{ message }}</p>
+  <button @click="updateHandle">修改数据</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello" 
+    }
+  },
+  methods: {
+    updateHandle() {
+      this.message = "World";
+    }
+  },
+  watch: {
+    message(newVal, oldVal) {
+      console.log(newVal, oldVal);
+    }
+  }
+}
+</script>
