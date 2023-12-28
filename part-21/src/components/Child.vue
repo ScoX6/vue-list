e9cdca6335b9dd2dd55affe5e2f20a64109b575f
xzf <yinuo@xzf.com>
2023-12-28 21:25:59 +0800

    update

---
+++ part-21/src/components/Child.vue
@@ -0,0 +1,20 @@
+<template>
+    <h3>Child</h3>
+    <p>{{ pInt }}</p>
+    <p>{{ pFloat }}</p>
+    <p>{{ pString }}</p>
+    <p v-for="item in pArray">{{ item }}</p>
+    <p v-for="(item, index) in pObject">{{ index }}: {{ item }}</p>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    props: [
+        "pInt", "pFloat", "pString", "pArray", "pObject"
+    ],
+}
+</script>
---
+++ part-21/src/components/Parent.vue
@@ -0,0 +1,27 @@
+<template>
+    <h3>Parent</h3>
+    <Child :pInt="pInt" :pFloat="pFloat" :pString="pString" :pArray="pArray" :pObject="pObject" />
+</template>
+<script>
+import Child from './Child.vue'
+export default {
+    data() {
+        return {
+            pInt: 12,
+            pFloat: 12.5,
+            pString: "Hello World",
+            pArray: [
+                "Hello",
+                "World"
+            ],
+            pObject: {
+                name: "John",
+                age: 25
+            }
+        }
+    },
+    components: {
+        Child
+    }
+}
+</script>
