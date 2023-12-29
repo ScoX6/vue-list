971eab3d01c0f8caa8ba028f5daf845a8406c827
xzf <yinuo@xzf.com>
2023-12-29 14:56:12 +0800

    update

---
+++ part-23/src/components/Child.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            msg: "Hello，parent"
+        }
+    },  
+    methods: {
+        clickEventHandle() {
+            console.log("clickEventHandle")
+            this.$emit("someEvent", this.msg)
+        }
+    }
+}
+</script>
+<template>
+    <h3>Child</h3>
+    <button @click="clickEventHandle">传递数据</button>
+</template>
---
+++ part-23/src/components/Parent.vue
@@ -0,0 +1,24 @@
+<script>
+import Child from './Child.vue';
+export default {
+    components: {
+        Child
+    },
+    methods: {
+        getHandle(msg) {
+            console.log("触发了")
+            this.message = msg
+        }
+    },
+    data() {
+        return {
+            message: ""
+        }
+    }
+}
+</script>
+<template>
+    <h3>组件事件</h3>
+    <Child @someEvent="getHandle"/>
+    <p>来自child的数据: {{ message }}</p>
+</template>
