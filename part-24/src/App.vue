5e426b6db8e8acbf7baa73f2c2678836266fb615
xzf <yinuo@xzf.com>
2023-12-29 15:15:25 +0800

    update

---
+++ part-24/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import MainComponent from './components/MainComponent.vue'
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    MainComponent,
+  }
+}
+</script>
+<template>
+  <h3>组件事件配合v-model</h3>
+  <MainComponent/>
+</template>
---
+++ part-24/src/components/MainComponent.vue
@@ -0,0 +1,23 @@
+<script>
+import SearchComponent from './SearchComponent.vue'
+export default {
+  data() {
+    return {
+      search: ""
+    }
+  },
+  components: {
+    SearchComponent
+  },
+  methods: {
+    getSearch(data) {
+        this.search = data
+    }
+  }
+}
+</script>
+<template>
+    <h3>Main</h3>
+    <SearchComponent @searchEvent="getSearch"/>
+    <p>搜索内容：{{ search }}</p>
+</template>
