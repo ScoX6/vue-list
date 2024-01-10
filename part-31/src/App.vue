edb49b00abb1ccfbed2fc3ca4b5f852642016611
xzf <yinuo@xzf.com>
2024-01-10 14:31:59 +0800

    update

---
+++ part-31/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <component :is="tabComponent"></component>
+  <button @click="switchTabComponent">切换</button>
+</template>
+<script>
+import { defineAsyncComponent } from 'vue';
+import ComponentA from './components/ComponentA.vue';
+const ComponentB = defineAsyncComponent(() => import('./components/ComponentB.vue'))
+export default {
+  data() {
+    return {
+      tabComponent: "ComponentA",
+    }
+  },
+  components: {
+    ComponentA,
+    ComponentB,
+  },
+  methods: {
+    switchTabComponent() {
+      this.tabComponent = this.tabComponent == "ComponentA" ? "ComponentB" : "ComponentA";
+    }  
+  },
+}
+</script>
---
+++ part-31/src/components/ComponentB.vue
@@ -0,0 +1,10 @@
+<template>
+    <h3>ComponentB</h3>
+</template>
+<script>
+export default {
+    data() {
+        return {}
+    },
+}
+</script>
