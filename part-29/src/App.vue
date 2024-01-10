aef6899bfbf73c4a9751db57e091780ba4334ad1
xzf <yinuo@xzf.com>
2024-01-10 13:59:41 +0800

    update

---
+++ part-29/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <component :is="tabComponent"></component>
+  <button @click="switchComponentHandle">切换</button>
+</template>
+<script>
+import ComponentA from './components/ComponentA.vue';
+import ComponentB from './components/ComponentB.vue';
+export default {
+  data() {
+    return {
+      tabComponent: "ComponentA",
+    }
+  },
+  components: {
+    ComponentA,
+    ComponentB,
+  },
+  methods: {
+    switchComponentHandle() {
+      if (this.tabComponent == "ComponentA") {
+        this.tabComponent = "ComponentB"
+      }else {
+        this.tabComponent = "ComponentA"
+      }
+    }
+  }
+}
+</script>
