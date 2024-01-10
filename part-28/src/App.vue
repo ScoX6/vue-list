122e51ad15816b929c5567942bf39152a2f8fa18
xzf <yinuo@xzf.com>
2024-01-10 11:28:13 +0800

    update

---
+++ part-28/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <UserComponent></UserComponent>
+</template>
+<script>
+import UserComponent from './components/UserComponent.vue';
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    UserComponent,
+  }
+}
+</script>
---
+++ part-28/src/components/UserComponent.vue
@@ -0,0 +1,68 @@
+<template>
+    <h3>组件生命周期函数应用</h3>
+    <p ref="name">P标签</p>
+    <ul>
+        <li v-for="(item, idnex) of users" :key="index">
+            <h3>{{ item.name }}</h3>
+            <p>gender: {{ item.gender }}</p>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            users: [
+
+            ],
+            fakeDatas: [
+            {
+                name: "Tom",
+                age: 12,
+                avatar: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww",
+                gender: "male",
+                hobby: [
+                    "PingPong", "Basketball", "Football"
+                ],
+            },
+            {
+                name: "Rose",
+                age: 12,
+                avatar: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww",
+                gender: "female",
+                hobby: [
+                    "PingPong", "Basketball", "Football"
+                ],
+            },
+            {
+                name: "Jack",
+                age: 12,
+                avatar: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww",
+                gender: "male",
+                hobby: [
+                    "PingPong", "Basketball", "Football"
+                ],
+            },
+            {
+                name: "Tim",
+                age: 12,
+                avatar: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww",
+                gender: "male",
+                hobby: [
+                    "PingPong", "Basketball", "Football"
+                ],
+            },
+            ],
+        }
+    },
+    beforeMount() {
+        console.log(this.$refs.name)
+    },
+    mounted() {
+        console.log(this.$refs.name)
+    },
+    created() {
+        this.users = this.fakeDatas
+    },
+}
+</script>
